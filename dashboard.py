06d8a9825e7b4a0bb8302980f92131b1c9a15bba
JoseBenaya <josebenaya@gmail.com>
2022-08-06 00:06:25 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,8 @@
 # Import Libraries
 import pandas as pd
+import matplotlib.pyplot as plt
 import plotly.express as px
+import plotly.graph_objects as go
 import pycountry
 import numpy as np
 import streamlit as st
